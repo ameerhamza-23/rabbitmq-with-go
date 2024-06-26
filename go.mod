f99de7d5a2fcb8327d65f5e753bdf395aa400d76
ameerhamza-23 <hamzaiscoding@gmail.com>
2024-06-26 21:17:26 +0500

    Added producer code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ameerhamza-23/rabbitmq-with-go
 
 go 1.22.4
+
+require github.com/streadway/amqp v1.1.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,60 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/streadway/amqp"
 )
 
 func main() {
-	fmt.Println("working")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to rabbitmq instance")
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	fmt.Println("q : ", q)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully published a message to queue")
+
 }
